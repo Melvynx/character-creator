a614e2e10cbe286464e3a37516a90eed30bec183
Melvyn M <56388157+Melvynx@users.noreply.github.com>
2021-10-20 17:55:38 +0200

    exercise 4: solution

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import BottomBackground from './components/BottomBackground';
 
 import CharacterEditor from './components/CharacterEditor';
 import Footer from './components/Footer';
@@ -7,7 +6,6 @@ import Footer from './components/Footer';
 function App() {
   return (
     <>
-      <BottomBackground />
       <CharacterEditor />
       <Footer />
     </>
--- src/components/BottomBackground/BottomBackground.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import styles from './BottomBackground.module.css';
-
-const BottomBackground = () => {
-  return <div className={styles.background} />;
-};
-
-export default BottomBackground;
